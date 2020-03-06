3a44b90310d11ac198251433e544d09d8379cf19
Tyler Phillips <phillity@umail.iu.edu>
2020-03-06 15:40:24 -0500

    Training working

--- config/config.py
+++ config/config.py
@@ -1,36 +1,30 @@
 class Config(object):
     env = "default"
+    dataset = "CIFAR10"
     backbone = "resnet18"
-    classify = "softmax"
-    num_classes = 10575
+
+    emb_feat_size = 512  # feature embedding size
     metric = "arc_margin"
-
-    train_root = "data/Datasets/lfw/lfw-align-128/" #"data/Datasets/CASIA-WebFace/CASIA-WebFace/"
-    train_list = "data/Datasets/lfw/train_data.txt" #"data/Datasets/CASIA-WebFace/train_data.txt"
-    val_list = "data/Datasets/lfw/valid_data.txt" #"data/Datasets/CASIA-WebFace/valid_data.txt"
-
-    test_root = "data1/Datasets/anti-spoofing/test/data_align_256"
-    test_list = "test.txt"
+    m = 0.35
+    s = 4
 
     checkpoints_path = "checkpoints"
-    load_model_path = "models/resnet18.pth"
-    test_model_path = "checkpoints/resnet18_30.pth"
-    train_model_path = "checkpoints/resnet18_30.pth"
+    model_path = "models/{}.pth".format(backbone)
     save_interval = 10
 
-    train_batch_size = 50  # batch size
-    test_batch_size = 60
+    train_batch_size = 64
+    test_batch_size = 64
 
     input_shape = (1, 128, 128)
 
     optimizer = "sgd"
 
-    use_gpu = False  # use GPU or not
+    use_gpu = False
     gpu_id = "0"
-    num_workers = 4  # how many workers for loading data
-    print_freq = 100  # print info every N batch
+    num_workers = 8
+    print_freq = 5
 
-    debug_file = "/tmp/debug"  # if os.path.exists(debug_file): enter ipdb
+    debug_file = "/tmp/debug"
     result_file = "result.csv"
 
     max_epoch = 50
--- train.py
+++ train.py
@@ -1,14 +1,27 @@
 import os
 import numpy as np
 import torch
-from torch.nn import DataParallel
-import torch.nn.functional as F
-from torch.optim.lr_scheduler import StepLR
-from torch.utils import data
-import torchvision
-import torchvision.transforms as transforms
-from models import *
-from config import Config
+from torch.nn import Module, CrossEntropyLoss, Linear, DataParallel
+from torch.optim import lr_scheduler, SGD, Adam
+from torch.utils.data import DataLoader, Sampler
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, CIFAR100
+from torchvision.models import resnet18, resnet34, resnet50
+from models.metrics import AddMarginProduct, ArcMarginProduct, SphereProduct
+from config.config import Config
+
+
+class ResNetFC(Module):
+    def __init__(self, in_features, out_features):
+        super(ResNetFC, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+
+    def forward(self, x):
+        if self.in_features == self.out_features:
+            return x
+        else:
+            return Linear(in_features=self.in_features, out_features=self.out_features)
 
 
 def save_model(model, save_path, name, iter_cnt):
@@ -24,47 +37,64 @@ if __name__ == "__main__":
     else:
         device = torch.device("cpu")
 
-    trainset = torchvision.datasets.CIFAR10(root="./data", train=True,
-                                            download=True, transform=transforms.Compose(
-                                                [transforms.ToTensor(),
-                                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
-    trainloader = data.DataLoader(trainset,
-                                  batch_size=opt.train_batch_size,
-                                  shuffle=True,
-                                  num_workers=opt.num_workers)
+    if opt.dataset == "CIFAR10":
+        trainset = CIFAR10(root="./data", train=True,
+                        download=True, transform=transforms.Compose(
+                            [transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+        valset = CIFAR10(root="./data", train=True,
+                           download=True, transform=transforms.Compose(
+                               [transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+    else:
+        trainset = CIFAR100(root="./data", train=True,
+                           download=True, transform=transforms.Compose(
+                               [transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+    num_classes = np.unique(trainset.targets).shape[0]
+    trainloader = DataLoader(trainset,
+                             batch_size=opt.train_batch_size,
+                             shuffle=True,
+                             num_workers=opt.num_workers)
 
-    print("{} train iters per epoch:".format(len(trainloader)))
-
-    criterion = torch.nn.CrossEntropyLoss()
+    print("Train iteration batch size: {}".format(opt.train_batch_size))
+    print("Train iterations per epoch: {}".format(len(trainloader)))
 
     if opt.backbone == "resnet18":
-        model = resnet18()
+        model = resnet18(pretrained=True)
+        model.fc = ResNetFC(512, opt.emb_feat_size)
     elif opt.backbone == "resnet34":
-        model = resnet34()
+        model = resnet34(pretrained=True)
+        model.fc = ResNetFC(512, opt.emb_feat_size)
     elif opt.backbone == "resnet50":
-        model = resnet50()
+        model = resnet50(pretrained=True)
+        model.fc = ResNetFC(512, opt.emb_feat_size)
 
     if opt.metric == "add_margin":
-        metric_fc = AddMarginProduct(512, opt.num_classes, device, s=30, m=0.35)
+        metric_fc = AddMarginProduct(
+            opt.emb_feat_size, num_classes, device, s=opt.s, m=opt.m)
     elif opt.metric == "arc_margin":
-        metric_fc = ArcMarginProduct(512, opt.num_classes, device, s=30, m=0.5)
+        metric_fc = ArcMarginProduct(
+            opt.emb_feat_size, num_classes, device, s=opt.s, m=opt.m)
     elif opt.metric == "sphere":
-        metric_fc = SphereProduct(512, opt.num_classes, device, m=4)
+        metric_fc = SphereProduct(
+            opt.emb_feat_size, num_classes, device, m=opt.m)
     else:
-        metric_fc = nn.Linear(512, opt.num_classes)
+        metric_fc = Linear(opt.emb_feat_size, num_classes)
 
     model.to(device)
     model = DataParallel(model)
     metric_fc.to(device)
     metric_fc = DataParallel(metric_fc)
 
+    criterion = CrossEntropyLoss()
     if opt.optimizer == "sgd":
-        optimizer = torch.optim.SGD([{"params": model.parameters()}, {"params": metric_fc.parameters()}],
+        optimizer = SGD([{"params": model.parameters()}, {"params": metric_fc.parameters()}],
                                     lr=opt.lr, weight_decay=opt.weight_decay)
     else:
-        optimizer = torch.optim.Adam([{"params": model.parameters()}, {"params": metric_fc.parameters()}],
+        optimizer = Adam([{"params": model.parameters()}, {"params": metric_fc.parameters()}],
                                      lr=opt.lr, weight_decay=opt.weight_decay)
-    scheduler = StepLR(optimizer, step_size=opt.lr_step, gamma=0.1)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=opt.lr_step, gamma=0.1)
 
     for i in range(opt.max_epoch):
         if i > 0:
@@ -88,8 +118,8 @@ if __name__ == "__main__":
                 output = output.data.cpu().numpy()
                 output = np.argmax(output, axis=1)
                 label = label.data.cpu().numpy()
-                acc = np.mean((output == label).astype(int))
-                print("train epoch {} iter {} loss {} acc {}".format(
+                acc = np.sum((output == label).astype(int)) / label.shape[0]
+                print("Epoch -- {} Iter -- {} Train Loss -- {:.6f} Train Acc -- {:.6f}".format(
                     i, ii, loss.item(), acc))
 
         if i % opt.save_interval == 0 or i == opt.max_epoch:
